44f47690710809e9cfc8b314102f31758489c844
hugovallada <valladahugo@gmail.com>
2021-04-13 10:06:51 -0300

    Adiciona DTO

--- src/main/java/com/github/hugovallada/forum/controller/TopicosController.java
+++ src/main/java/com/github/hugovallada/forum/controller/TopicosController.java
@@ -1,24 +1,25 @@
 package com.github.hugovallada.forum.controller;
 
+import com.github.hugovallada.forum.controller.dto.TopicoDto;
 import com.github.hugovallada.forum.modelo.Curso;
 import com.github.hugovallada.forum.modelo.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
-        Topico topico = new Topico("Duvida ", "Duvida com Spring", new Curso("Spring", "Programação"));
+    public List<TopicoDto> lista(){
+        Topico topico = new Topico("Dúvida ", "Duvida com Spring", new Curso("Spring", "Programação"));
 
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 
 }
