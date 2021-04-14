5b2ed658f85aa467412b1a8a5b1eac62d78e9525
hugovallada <valladahugo@gmail.com>
2021-04-14 11:06:11 -0300

    Adiciona o Spring Security e cria a classe de configuração

---
+++ src/main/java/com/github/hugovallada/forum/config/aecurity/SecurityConfigurations.java
@@ -0,0 +1,10 @@
+package com.github.hugovallada.forum.config.aecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+}
