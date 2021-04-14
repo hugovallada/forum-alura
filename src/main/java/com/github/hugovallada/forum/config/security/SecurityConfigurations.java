2be73d2b21e34c2f277d0d1fdd7ff42be5f70fef
hugovallada <valladahugo@gmail.com>
2021-04-14 14:22:46 -0300

    Cria o JWT

--- src/main/java/com/github/hugovallada/forum/config/security/SecurityConfigurations.java
+++ src/main/java/com/github/hugovallada/forum/config/security/SecurityConfigurations.java
@@ -1,8 +1,10 @@
 package com.github.hugovallada.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -46,5 +48,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     }
 
-
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 }
--- src/main/java/com/github/hugovallada/forum/controller/AutenticacaoController.java
+++ src/main/java/com/github/hugovallada/forum/controller/AutenticacaoController.java
@@ -1,7 +1,13 @@
 package com.github.hugovallada.forum.controller;
 
+import com.github.hugovallada.forum.config.security.TokenService;
 import com.github.hugovallada.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +19,23 @@ import javax.validation.Valid;
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
-        System.out.println(form.getEmail()+ "   " +form.getSenha());
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 
-        return ResponseEntity.ok().build();
+        try{
+            Authentication authentication = authenticationManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+            System.out.println(token);
+            return ResponseEntity.ok().build();
+        } catch (AuthenticationException e){
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
