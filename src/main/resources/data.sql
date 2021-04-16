8aeb970b186687e7391a6b179daa6fb8684d9d29
hugovallada <valladahugo@gmail.com>
2021-04-16 09:51:11 -0300

    Adiciona o @Profiles nas classes de segurança

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,12 @@
+INSERT INTO PERFIL VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$/mYur4zFSjkrYzsrlu.4NeGd2THq21.TpgdsnB0BAUWrTh2/iv96W');
+
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$/mYur4zFSjkrYzsrlu.4NeGd2THq21.TpgdsnB0BAUWrTh2/iv96W');
+
+INSERT INTO USUARIO_PERFIS VALUES (1,2);
+INSERT INTO USUARIO_PERFIS VALUES (2,1);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
