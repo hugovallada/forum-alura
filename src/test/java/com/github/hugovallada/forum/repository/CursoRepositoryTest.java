7913d251d230696caccfd40f36008bc4cab4d432
hugovallada <valladahugo@gmail.com>
2021-04-16 10:20:42 -0300

    Separa os bancos e profiles para teste

--- src/test/java/com/github/hugovallada/forum/repository/CursoRepositoryTest.java
+++ src/test/java/com/github/hugovallada/forum/repository/CursoRepositoryTest.java
@@ -4,18 +4,31 @@ import com.github.hugovallada.forum.modelo.Curso;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void deveriaCarregarUmCursoAoBuscaRPeloSeuNome(){
         String nomeCurso = "HTML 5";
+        Curso html7 = new Curso();
+        html7.setNome(nomeCurso);
+        html7.setCategoria("Prod");
+        em.persist(html7);
+
         Curso curso = repository.findByNome(nomeCurso);
 
         Assertions.assertNotNull(curso);
